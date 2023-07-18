e7179c169fe595e8b81f1686e4c36a677bcd0ad9
nuflix <djordje.vujicic@yandex.com>
2023-07-18 15:43:47 +0200

    itle font size 26

--- CryptoWallet/CryptoWallet/View/Components/TitleTextComponentView.swift
+++ CryptoWallet/CryptoWallet/View/Components/TitleTextComponentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TitleTextComponentView: View {
     let title: String
     var body: some View {
-        Text(title).bold().foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0)).font(.system(size: 36))
+        Text(title).bold().foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0)).font(.system(size: 26))
     }
 }
 
