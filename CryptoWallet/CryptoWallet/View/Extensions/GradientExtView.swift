ff9c4aa917a3037ad07554058cb72cac5ec75407
nuflix <djordje.vujicic@yandex.com>
2023-07-19 11:04:42 +0200

    review changes

---
+++ CryptoWallet/CryptoWallet/View/Extensions/GradientExtView.swift
@@ -0,0 +1,8 @@
+//
+//  Gradient.swift
+//  CryptoWallet
+//
+//  Created by Administrator on 7/19/23.
+//
+
+import Foundation
---
+++ CryptoWallet/CryptoWallet/View/Extensions/RoundedCornersExtView.swift
@@ -0,0 +1,8 @@
+//
+//  RoundedCornersExtView.swift
+//  CryptoWallet
+//
+//  Created by Administrator on 7/19/23.
+//
+
+import Foundation
